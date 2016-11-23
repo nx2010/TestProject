db2c8330610e0b5fe0737043eebc3f5ad2eb4271
bkortmann <bkortmann84@gmail.com>
2016-11-23 00:20:15 -0800

    Second Commit

---
+++ app/app.module.ts
@@ -0,0 +1,42 @@
+import { FormsModule } from '@angular/forms';
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { Data, RouterModule } from '@angular/router';
+
+import { AppComponent }   from './app.component';
+import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent } from './heroes.component';
+import { HeroService } from './hero.service';
+import { DashboardComponent } from './dashboard.component';
+
+@NgModule({
+  imports:      [ 
+    BrowserModule, 
+    FormsModule,
+    RouterModule.forRoot([
+        {
+          path: '',
+          redirectTo: '/dashboard',
+          pathMatch: 'full'
+        },
+        {
+          path: 'heroes',
+          component: HeroesComponent
+        },
+        {
+          path: 'detail/:id',
+          component: HeroDetailComponent
+        },
+        {
+          path: 'dashboard',
+          component: DashboardComponent
+        }
+      ]) 
+    ],
+  declarations: [ AppComponent, HeroDetailComponent, HeroesComponent, DashboardComponent ],
+  providers:    [ HeroService ],
+  bootstrap:    [ AppComponent ]
+})
+
+
+export class AppModule {}
